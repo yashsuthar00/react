13260db77d2a16a45558b23a111ad79ac5bc241b
YashSuthar <yashsuthar0309@gmail.com>
2024-11-16 01:18:13 +0530

    day1
    basic component cration

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,10 @@
+function Chai() {
+    return (
+        <div>
+            <h1>Chai</h1>
+            <p>Chai is a popular Indian drink</p>
+        </div>
+    )
+}
+
+export default Chai;
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,12 @@
+
+
+function Chai() {
+    return (
+        <div>
+            <h1>Chai</h1>
+            <p>Chai is a popular Indian drink</p>
+        </div>
+    )
+}
+
+export default Chai;
