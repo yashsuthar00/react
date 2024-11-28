bd21fb6eb23a5749f5aa412b0e7e89ef4facdb59
YashSuthar <yashsuthar0309@gmail.com>
2024-11-28 21:29:16 +0530

    day 5
    custom hooks

---
+++ 06currencyconvertor/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-green-600 bg-black'>Hello world</h1>
+    </>
+  )
+}
+
+export default App
